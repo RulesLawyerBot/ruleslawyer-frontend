98133ef56f9e6f0463c23bf9698b187219d4dcb6
gold-mir <miranda.keith.42@gmail.com>
2021-05-01 16:07:57 -0600

    rules displaying now

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+
+import { Fragment } from 'react'
+
 import SearchBox from './components/searchBox'
+import RuleDisplay from './components/ruleDisplay'
 
 interface State {
-    displayedRules: RuleData[]
+    displayedRules: RuleData[],
+    statusMessage: string
 }
 
 interface RLReturnData {
@@ -16,7 +21,7 @@ interface RLReturnData {
     rules: RuleData[]
 }
 
-interface RuleData{
+export interface RuleData{
     parentText: string,
     ruleSource: string,
     subRules: RuleData[],
@@ -26,7 +31,8 @@ interface RuleData{
 export class App extends React.Component<{}, State> {
 
     state: State = {
-        displayedRules: []
+        displayedRules: [],
+        statusMessage: null
     }
 
     private async getRules(query: string): Promise<void> {
@@ -36,22 +42,31 @@ export class App extends React.Component<{}, State> {
         }
         url.search = new URLSearchParams(params).toString()
 
-        console.log(`fetching from ${url}`)
 
         let response = await fetch(url.href)
         let body: any = await response.json()
         let data: RLReturnData = body as RLReturnData
 
-        let newRules: RuleData[] = data.rules
+        if(data.rules.length > 0) {
+            let newRules: RuleData[] = data.rules
+            this.setState({displayedRules: newRules, statusMessage: `Showing ${data.rules.length} results for "${query}".`})
+        } else {
+            this.setState({statusMessage: `Search for "${query}" returned no results.`})
+        }
 
-        this.setState({displayedRules: newRules})
-
+        console.log(this.state.statusMessage)
         console.log(this.state.displayedRules)
     }
     
     render(): React.ReactElement {
         return (
-            <SearchBox onSubmit={this.getRules.bind(this)}/>
+            <Fragment>
+                <SearchBox onSubmit={this.getRules.bind(this)}/>
+                <h5>{this.state.statusMessage}</h5>
+                <div id="results">
+                    {this.state.displayedRules.map((rule: RuleData, index: number) => <RuleDisplay key={index.toString()} rule={rule}/>)}
+                </div>
+            </Fragment>
         )
     }
 
---
+++ src/components/ruleDisplay.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { RuleData } from '../app'
+import { Fragment } from 'react'
+
+
+interface Props {
+    rule: RuleData
+}
+
+function RuleDisplay(props: Props): React.ReactElement {
+    const { parentText, subRules, ruleSource, text }: RuleData = props.rule
+    
+    let header: string = parentText? parentText: text
+
+    let body: React.ReactElement
+
+    if(subRules.length === 0) {
+        body = <div><p>{text}</p></div>
+    } else {
+        body = <div>
+            {subRules.map((rule: RuleData, index: number) => {
+                if(rule.subRules.length === 0){
+                    return <p key={index.toString()}>{rule.text}</p>
+                } else {
+                    return (
+                        <Fragment key={index.toString()}>
+                            <h4>{rule.text}</h4>
+                            {rule.subRules.map((subRule: RuleData, subIndex: number) => <p key={`sub${subIndex.toString()}`}>{subRule.text}</p>)}
+                        </Fragment>
+                    )
+                }
+            })}
+        </div>
+    }
+    
+    return (
+        <div>
+            <h3>{header}</h3>
+            {body}
+        </div>
+    )
+}
+
+export default RuleDisplay
