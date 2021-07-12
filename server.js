e754c20baaf1a5130d6edd7ec6546025afc38d19
gold-mir <miranda.keith.42@gmail.com>
2021-07-12 16:21:29 -0600

    update server.js to fix react router issue

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 3000 // Heroku will need the PORT environment v
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+
 app.listen(port, () => console.log(`App is live on port ${port}!`))
