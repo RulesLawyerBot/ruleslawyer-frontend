330ea3b3fb1e5027febf9f7968b81ff86a39896d
gold-mir <miranda.keith.42@gmail.com>
2021-11-17 14:14:48 -0700

    add result count, not found messages to search page

--- src/components/searchPage.tsx
+++ src/components/searchPage.tsx
@@ -71,16 +71,28 @@ const SearchPage: React.FunctionComponent<{}> = (): React.ReactElement => {
         setLoading(false)
         if(data.rules.length > 0) {
             setRules(data.rules)
-            setMessage(`Showing ${data.rules.length} results for ${query}.`)
+            setMessage(`Showing ${data.rules.length} results for "${query}".`)
             setKeywords(data.request.keywords)
         } else {
-            setMessage(`Search for ${query} returned no results`)
+            setMessage('')
         }
     }
 
     useEffect((): void => {
         fetchRules()
     }, [query])
+
+    let notFound: React.ReactElement
+
+    if(!loading && rules.length === 0) {
+        notFound = (
+            <div className={CSS.notFound}>
+                <h3>No results found for "{query}".</h3>
+                <p>RulesLawyer searches the following rules documents: the Comprehensive Rules, Infraction Procedure Guide, Magic Tournament Rules, Judging at Regular document, Oathbreaker rules, Digital Infraction Procedure Guide, and Digital Magic Tournament rules.</p>
+                <p>Much like a traditional search engine, if your query does not appear in these documents, no results will be returned. It is suggested that you only use words that you think are likely to appear in these documents.</p>
+            </div>
+        )
+    }
 
     return (
         <div>
@@ -89,7 +101,8 @@ const SearchPage: React.FunctionComponent<{}> = (): React.ReactElement => {
                 <SearchBox/>
             </div>
             <div className={CSS.body}>
-                {/* {message? message : null} */}
+                {message? <div className={CSS.message}><p>{message}</p></div> : null}
+                {notFound}
                 {loading? <div className={CSS.loading}><div className={CSS.loadingAnimation}></div><div className={CSS.loadingText}>Loading</div></div>: null}
                 <ResultsList rules={rules} keywords={keywords}/>
             </div>
