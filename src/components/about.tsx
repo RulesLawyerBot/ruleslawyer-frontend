f12efae73722977c799a2c59fc4ab1cca2d72db4
Elaine Cao <elaine.cao.93@gmail.com>
2022-04-24 09:22:38 -0600

    credit yourself nerd

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -8,7 +8,7 @@ const About: React.FunctionComponent<{}> = (): React.ReactElement => {
         <div>
             <h1><Link to='/'><img src="favicon.png" alt="OWL" width="30"/></Link> About RulesLawyer</h1>
             <div className={CSS.textBody}>
-                <p>RulesLawyer is written by <b><a href="https://judgeacademy.com/user/profile/oritart/">Elaine Cao</a></b>, Level 2, and <b>Miranda Keith</b>, from Calgary, Alberta.</p>
+                <p>RulesLawyer is written by <b><a href="https://judgeacademy.com/user/profile/oritart/">Elaine Cao</a></b>, Level 2, and <b><a href="https://judgeacademy.com/user/profile/miraweave/">Miranda Keith</a></b>, Level 1, from Calgary, Alberta.</p>
                 <h2>Special thanks</h2>
                 <p><b><a href="https://vyseri.com/">Tobias Vyseri</a></b>, Level 2, for the owl logo.</p>
                 <p><b>The <a href="https://www.scryfall.com">Scryfall</a> team</b> for their card API, as used in the Discord bot.</p>
